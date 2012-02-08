572eaa2e7bb90d9755c661a494e8547bc651ef20
teiikushi07 <teiikushi07@gmail.com>
2012-02-08 15:44:14 +0800

    新增地圖編輯功能
    index.py	新增"從SDB讀出地圖資料和更新地圖資料"的功能，新增少許註解

--- flaskapp/index.py
+++ flaskapp/index.py
@@ -14,30 +14,57 @@ def map_list():
     maps = dom.select(query="select mid, name, width, height from `game-maps`")
     return render_template('map_list.html', maps=maps)
 
-@app.route('/maps/add/', methods=['GET', 'POST'])
-def map_add():
+def map_editor(mid=None):
+    #將map存於SDB
     if request.method == 'POST':
         map_data = json.loads(request.form.get('map'))
-        
-        domain_meta = sdb_con.domain_metadata(dom)
-        mid = domain_meta.item_count+1
-        PK = 'map_' + str(mid)
-        grid_json = json.dumps(map_data['grid'])
-        grid_part = 1 if len(grid_json) < 1000 else (len(grid_json)//1000)+1
-        attrs = {
-                 'mid': mid,
-                 'name': request.form.get('name', '無標題'),
-                 'width': map_data['width'],
-                 'height': map_data['height'],
-                 'grid_part': grid_part,
-                 }
-        for part in xrange(grid_part):
-            attrs['grid_'+ str(part+1)] = grid_json[part*1000:(part+1)*1000]
-        dom.put_attributes(item_name=PK, attributes=attrs, replace=False)
-        return redirect(url_for('map_list'))
+        if mid:
+            map_item = dom.get_item('map_'+str(mid), consistent_read=True)
+            map_item['name'] = request.form.get('name', '無標題')
+            grid_json = json.dumps(map_data['grid'])
+            for part in xrange(map_item['grid_part']):
+                map_item['grid_'+ str(part+1)] = grid_json[part*1000:(part+1)*1000]
+            map_item.save(replace=False)
+        else:
+            #取得在SDB 上item 的數量
+            domain_meta = sdb_con.domain_metadata(dom)
+            mid = domain_meta.item_count+1
+            PK = 'map_' + str(mid)
+            grid_json = json.dumps(map_data['grid'])
+            #計算grid所需的拆分後的grid_part數目
+            grid_part = 1 if len(grid_json) < 1000 else (len(grid_json)//1000)+1
+            attrs = {
+                     'mid': mid,
+                     'name': request.form.get('name', '無標題'),
+                     'width': map_data['width'],
+                     'height': map_data['height'],
+                     'grid_part': grid_part,
+                     }
+            #將grid 拆成 grid_1, grid_2,...
+            for part in xrange(grid_part):
+                attrs['grid_'+ str(part+1)] = grid_json[part*1000:(part+1)*1000]
+            dom.put_attributes(item_name=PK, attributes=attrs, replace=False)
+            return redirect(url_for('map_list'))
+    #從SDB 中取得map
     else:
-        args = {
-                'width': request.args.get('w', 10, type=int),
-                'height': request.args.get('h', 10, type=int)
-                }
-        return render_template('map_editor.html', args=args)
+        if mid:
+            map_item = dom.get_item('map_'+str(mid), consistent_read=True)
+            grid_json = ''
+            for part in xrange(map_item['grid_part']):
+                grid_json += map_item['grid_'+str(part+1)]
+            args = {
+                    'width': map_item['width'],
+                    'height': map_item['height'],
+                    'name': map_item['name'],
+                    }
+            return render_template('map_editor.html', args=args, grid=grid_json)
+        else:
+            args = {
+                    'width': request.args.get('w', 10, type=int),
+                    'height': request.args.get('h', 10, type=int),
+                    'name': '無標題'
+                    }
+            return render_template('map_editor.html', args=args)
+
+app.add_url_rule('/maps/edit/<int:mid>', 'map_edit', map_editor, methods=['GET', 'POST'])
+app.add_url_rule('/maps/add/', 'map_add', map_editor, methods=['GET', 'POST'])
